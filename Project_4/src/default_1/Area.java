0da50bc6f4f12994c7a08a32448150d691e5c7dc
tejhande <59686002+tejhande@users.noreply.github.com>
2024-07-29 11:27:57 +0530

    29-07-24

---
+++ Project_4/src/default_1/Area.java
@@ -0,0 +1,26 @@
+package default_1;
+
+import java.util.Scanner;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int side = 20;
+//		System.out.println("Area Of Sqaure:- "+side*side);
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Length: ");
+		int l=sc.nextInt();
+//		System.out.println("Enter Breadth: ");
+//		int b=sc.nextInt();
+		int area = l*l;
+		System.out.println("Area Of Rectangle:- "+area);
+		
+		
+		
+		
+	}
+
+}
+ 
