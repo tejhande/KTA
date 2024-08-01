fa90a0883e56aa0c2e668973408e1030f0d4d5bf
tejhande <59686002+tejhande@users.noreply.github.com>
2024-08-01 11:55:31 +0530

    01-08-24

---
+++ Project_7/src/default_1/WeekDays.java
@@ -0,0 +1,38 @@
+package default_1;
+
+import java.util.Scanner;
+
+public class WeekDays {
+
+	public static void main(String[] args) {
+		String a = "Monday";
+		String b = "Tuesday";
+		String c = "Wednesday";
+		String d = "Thursday";
+		String e = "Friday";
+		String f = "Saturday";
+		String g = "Sunday";
+		
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter No. Of Day:- ");
+		int number = myObj.nextInt();
+		
+		if (number == 1)
+			System.out.println("Monday");
+		else if (number == 2)
+			System.out.println("Tuesday");
+		else if (number == 3)
+			System.out.println("Wednesday");
+		else if (number == 4)
+			System.out.println("Thursday");
+		else if (number == 5)
+			System.out.println("Firday");
+		else if (number == 6)
+			System.out.println("Saturday");
+		else if (number == 7)
+			System.out.println("Sunday");
+		else
+			System.out.println("Wrong Input !");
+	}
+
+}
