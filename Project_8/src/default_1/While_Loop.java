0b6f3004aa11d18643fdacffacadb7bb51eaa1ba
tejhande <59686002+tejhande@users.noreply.github.com>
2024-08-02 11:31:26 +0530

    02-08-24

---
+++ Project_8/src/default_1/While_Loop.java
@@ -0,0 +1,15 @@
+package default_1;
+
+public class While_Loop {
+
+	public static void main(String[] args) {
+		int i =1;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
---
+++ Project_8/src/default_1/While_Loop20_30.java
@@ -0,0 +1,15 @@
+package default_1;
+
+public class While_Loop20_30 {
+
+	public static void main(String[] args) {
+		int i =20;
+		while(i<=30)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
