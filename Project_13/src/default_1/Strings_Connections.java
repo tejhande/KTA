fb68f256d9f67ca5a2f21846563ec9902623b83d
tejhande <59686002+tejhande@users.noreply.github.com>
2024-08-09 22:47:27 +0530

    09-08-24

---
+++ Project_13/src/default_1/Strings_Connections.java
@@ -0,0 +1,14 @@
+package default_1;
+
+public class Strings_Connections {
+
+	public static void main(String[] args) {
+		int num1 = 101;
+		int num2 = 25;
+		System.out.println("The sum of "+num1 +" + " +num2 +" is "+ num1+num2);
+		System.out.println("The susbtraction of "+num1 +" - " +num2 +" is "+ (num1-num2));
+		System.out.println("The multiplication of "+num1 +" * " +num2 +" is "+ num1*num2);
+		System.out.println("The Division of "+num1 +" / " +num2 +" is "+ num1/num2);
+		}
+
+}
