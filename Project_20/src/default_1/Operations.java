b3c9ca2b445d4f3d94d644d0b1c9435a179b0a55
tejhande <59686002+tejhande@users.noreply.github.com>
2024-08-27 11:18:47 +0530

    27-08-24

---
+++ Project_20/src/default_1/Operations.java
@@ -0,0 +1,27 @@
+package default_1;
+
+public class Operations {
+	void add() {
+		int a =10, b = 20;
+		int sum = a+b;
+		System.out.println("The total is "+sum);
+	}
+	void add(int a,int b) {
+		int sum = a+b;
+		System.out.println("The total is "+sum);
+	}
+	void add(int a,int b, int c) {
+		int sum = a+b+c;
+		System.out.println("The total is "+sum);
+	}
+	void add(float a,float b) {
+		float sum = a+b;
+		System.out.println("The total is "+sum);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project_20/src/default_1/Operations_1.java
@@ -0,0 +1,19 @@
+package default_1;
+
+
+public class Operations_1 {
+
+	
+	public static void main(String[] args) {
+	
+		Operations o1 = new Operations();
+		o1.add();
+		o1.add(10.2f, 15.4f);
+		o1.add(5, 60);
+		o1.add(252, 250, 560);
+		
+		
+		
+	}
+
+}
